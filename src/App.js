002a61d06f59dee2592a2d0086c22de4fcf8a2c3
watermelon <81776676+rtjessica@users.noreply.github.com>
2023-02-06 20:41:10 -0500

    added footer with github link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ function App() {
         <h2>Weather App</h2>
         <Search />
       </header>
+      <footer>
+        <a
+          href="https://github.com/rtjessica/weather-app-react"
+          target="_blank"
+          rel="noreferrer">
+          Open-source code
+        </a>
+        <span>by Jess Teixeira</span>
+      </footer>
     </div>
   );
 }
